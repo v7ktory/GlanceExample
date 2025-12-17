cb84a074be62e734c4674924637442ba1a5f0290
v7ktory <v7ktory@gmail.com>
2025-12-17 21:53:01 +0700

    added periodic stock price updates in widget

--- app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -1,7 +1,6 @@
 package com.example.glanceexample.glance
 
 import android.content.Context
-import androidx.glance.text.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
@@ -9,13 +8,30 @@ import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.updateAll
 import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
+import androidx.glance.text.Text
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration
+
 class StockAppWidget : GlanceAppWidget() {
 
+    private var job: Job? = null
+
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        if (job == null) {
+            job = startUpdateJob(
+                Duration.ofSeconds(20).toMillis(),
+                context
+            )
+        }
 
         provideContent {
             GlanceTheme {
@@ -24,14 +40,26 @@ class StockAppWidget : GlanceAppWidget() {
         }
     }
 
-    @Composable
-    fun GlanceContent() {
-        Column(modifier = GlanceModifier
+    private fun startUpdateJob(timeInterval: Long, context: Context): Job {
+        return CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                PriceDataRepo.update()
+                StockAppWidget().updateAll(context)
+                delay(timeInterval)
+            }
+        }
+    }
+}
+
+
+@Composable
+fun GlanceContent() {
+    Column(
+        modifier = GlanceModifier
             .fillMaxSize()
             .background(GlanceTheme.colors.background)
             .padding(8.dp)
-        ) {
-            Text("Demo")
-        }
+    ) {
+        Text("Demo")
     }
 }
